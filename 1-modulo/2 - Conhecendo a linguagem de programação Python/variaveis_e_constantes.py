44bb3b4cc507047d8993636d77154d256d4ad418
Luucio <lucioflaviocontatopro@gmail.com>
2023-08-22 23:14:52 -0300

    Conclusão aula Variáveis e Constantes

---
+++ 1-modulo/2 - Conhecendo a linguagem de programação Python/variaveis_e_constantes.py
@@ -0,0 +1,9 @@
+#exemplo variaveis em Python
+    #modelo normal
+age = 23;
+name = 'Guilherme'
+print(f'Meun nome é {name} e eu tenho {age} ano(s) de idade.')
+    #modelo python
+age, name = (23, 'Guilherme')
+print(f'Meun nome é {name} e eu tenho {age} ano(s) de idade.')
+
