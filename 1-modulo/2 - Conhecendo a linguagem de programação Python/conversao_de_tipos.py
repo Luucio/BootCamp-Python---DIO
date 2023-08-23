5bd0a54e7d3dc36777a8f786e41e14b0716d629d
Luucio <lucioflaviocontatopro@gmail.com>
2023-08-22 23:33:03 -0300

    Conclusão aula conversão de tipos

---
+++ 1-modulo/2 - Conhecendo a linguagem de programação Python/conversao_de_tipos.py
@@ -0,0 +1,23 @@
+#inteiro para float
+preco = 10
+print(preco)
+
+preco = float(preco)
+print(preco)
+
+print('==============================================')
+
+#flot para inteiro
+preco1 = 10.0
+print(preco1)
+
+preco1 = int(preco1)
+print(preco1)
+
+print('==============================================')
+##concatena
+idade = 28
+preco2 = 50
+        # o "f" serve para avisar que ira colocar variaveis no print
+print(f"idade {idade} preco {preco2}")
+
