19ecc7f2cb3c1a428c06474978e8fb650284fd0d
Luucio <lucioflaviocontatopro@gmail.com>
2023-08-22 23:50:08 -0300

    Andamento de Estudo 22/08/2023 23:50

---
+++ 1-modulo/2 - Conhecendo a linguagem de programação Python/funcao_entrada_e_saida.py
@@ -0,0 +1,11 @@
+#input
+nome = input("informe o seu nome:")
+print(nome)
+
+#print
+nome = 'Guilherme'
+sobrenome = 'Carvalho'
+
+print(nome, sobrenome)
+print(nome, sobrenome, end="...\n") #\n serve como quebra de linha
+print(nome, sobrenome, sep="#")
